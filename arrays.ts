45bb495bef9bd94720d8956f9ebf09c94db120f8
RizovaD <donika.rizova@atscale.com>
2025-02-27 22:18:12 +0200

    lecture 6 array and functional

---
+++ arrays.ts
@@ -0,0 +1,38 @@
+let  numbersArray: number[] = [1, 2, 3, 4, 5];
+let stringsArray: string[] = ["apple", "Bannana", "Pear", "Peach", "Grape"];
+let stringAndNumber: (number | string)[] = ["Pesh", "Gosho", 2, "3"];
+
+let fruits: string[] = ["apple", "Bannana", "Pear", "Peach", "Grape"];
+let frutiesE1: string = fruits[1];
+let secondE2: string = fruits[3];
+
+//add to array
+fruits.push("mango");
+
+//delete last element from array
+numbersArray.pop();
+console.log("fruits", fruits);
+
+// delete from first position
+fruits.shift()
+
+//added in first positon
+numbersArray.unshift(5);
+
+//method MAP
+
+let multiplNumbers = numbersArray.map((element) => element * 2);
+
+console.log("multyplayNumbers", multiplNumbers);
+
+
+let multyplayNumbers = numbersArray.map((element: number) => 
+{{
+  if (element > 2) {
+    return element *2;
+  } else{
+    return element;
+  }}
+});
+
+let filterNumsArray = numbersArray.filter((element) => element > 2);
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 // first console log
-console.log("Hello TypeScript");
+//console.log("Hello TypeScript");
 
 //let VS const 
 
 let number: number = 5;
 number = 10;
-console.log("number", number)
+//console.log("number", number)
 
 const message: string = 'Welcome'
 
@@ -16,13 +16,13 @@ let welcomeMessage: string = "Hello, Donika!"
 let test: string = 'Rizova';
 
 let welcomeMessageLenght = welcomeMessage.length;
-console.log("welcomMessageLenght",welcomeMessageLenght);
+//console.log("welcomMessageLenght",welcomeMessageLenght);
 
 
 // String interplolation
 let welcommessageDetais: string = `My welcome message lenght is ${welcomeMessageLenght}`;
 let userWelcomeMessage: string = `Welcome, ${test}`
-console.log(userWelcomeMessage);
+//console.log(userWelcomeMessage);
 
 // Number 
 let firstNumber: number = 5;
@@ -33,7 +33,7 @@ let substract = firstNumber - secondNumber;
 let add = firstNumber + secondNumber;
 let multypl = firstNumber * secondNumber;
 let drvide = firstNumber / secondNumber;
-console.log("result", add)
+//console.log("result", add)
 
 // Boolen (true/false)
 
@@ -52,6 +52,46 @@ stringValue = 10;
 stringValue = 10 === 10;
 stringValue = [10, 10, 10];
 
-console.log("Is number or equal to", stringValue);
+//console.log("Is number or equal to", stringValue);
+
+// Ternary Operator
+
+const isPositiveNum = 5 > 0 ? "The number is positive" : "The numer is negative"
+//console.log(typeof isPositiveNum);
 
 
+const age: number = 45;
+const myName: string = "Donika"
+const personalDescription = age < 40 
+? `${myName} is younger them 40. She is ${age} years old.` 
+: `${myName} is older then 40. She is ${age}`;
+
+const personalName = `Her name is "${myName}".`
+
+//console.log("personalName", personalName)
+
+// If else Statement
+
+if(age < 40){
+console.log('Age are lower then 40')
+} else {
+//console.log("Age are grater then 40")
+};
+
+const num1: number = 400;
+
+if(num1 <= 10){
+console.log("The number is lower then 10.")
+} else if (num1 >100){
+console.log("The number is grater than 100.")
+} else if (num1 > 80){
+  console.log("The numeber is lower then 100");
+} else{
+  console.log("The number is between 10 and 100")
+}
+
+//Type Alias
+type personInfoType = string | number
+
+const personNameInfo: personInfoType = "Ivan"
+const personAnggInf: personInfoType = 25;
